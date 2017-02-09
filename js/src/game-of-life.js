57cc4624dd64be4089ca6f1147b041d91cc974eb
bryce-klinker <bklinker@pillartechnology.com>
2017-02-09 00:50:26 -0600

    First all dead generation

---
+++ js/src/game-of-life.js
@@ -0,0 +1,9 @@
+export class GameOfLife {
+    get generation() {
+        return [
+            [false, false, false],
+            [false, false, false],
+            [false, false, false]
+        ]
+    }
+}
--- js/src/game-of-life.spec.js
+++ js/src/game-of-life.spec.js
@@ -1,5 +1,12 @@
+import { GameOfLife } from './game-of-life';
+
 describe('Game of Life', () => {
     it('should have dead generation', () => {
-
+        var game = new GameOfLife();
+        expect(game.generation).toEqual([
+            [false, false, false],
+            [false, false, false],
+            [false, false, false]
+        ])
     })
 });
